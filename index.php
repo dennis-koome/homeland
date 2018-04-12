a65a84fd560955b8cd4ba0fc751b332dc35b7c4d
unknown <dkoome55@gmail.com>
2018-04-12 13:59:17 +0300

    added index.php

---
+++ index.php
@@ -0,0 +1,320 @@
+
+<?php
+include "templates/headermain.php";
+?>
+
+<script src="javascript/bests.js"></script>
+<script src="javascript/best_sellers.js"></script>
+
+<div id="wrapper"> 
+
+
+<style>
+.submit{margin-top: 10px;}
+</style>
+
+
+<div id="content">
+
+	<div id="welcome" class="con">
+		<div class="row">
+			<div class="six">
+				<h1>WELCOME TO HOMELAND SHELTER DEVELOPMENT IN KENYA</h1>
+				<P>TOMMORROW BEGINS TODAY</P>
+			</div>
+			
+			<div class="six">
+				<p>Homeland Shelter Development has grown to be a well established market in kenya since it started. 
+				We offer leading advice and transaction support to investors, corporates and homeowners for their personal 
+				and business property needs.</p>
+
+			   <p>Whether you are looking for a resident house, a land or a commercial building, we offer you the services you need
+				until you are more than satisfied. 
+				</p>
+			</div>
+		</div>
+		
+		
+		
+		
+		
+		<div id="clear">
+		</div>
+		
+	</div><!--end of welcome-->
+
+
+
+
+
+
+
+	<div id="servicepane" class="con">
+
+	<h2><span>Our Services</span></h2>
+		<div class="row">
+			<div id="conv" class="three" style="background-image: url('IMAGES/conveyancing.png');">
+				<h3>Title Conveyancing</h3>
+
+				<p>We transfer legal title of real property to the owners whether a group company or individual owners</p>
+
+			</div><!--end of the four part-->
+
+
+			<div class="three" style="background-image: url('IMAGES/title.png');">
+				<h3>Title Processing</h3>
+
+				<p></p>
+
+			</div><!--end of the four part-->
+
+
+			<div id='divconts' class="three" style="background-image: url('IMAGES/de.png'); ">
+				<h3>Land Subdivision Contracts</h3>
+
+				<p></p>
+
+			</div><!--end of the four part-->
+
+
+			<div class="three" style="background-image: url('IMAGES/gensupp.png');">
+				<h3>General Supplies and Commission Agents</h3>
+				<p></p>
+
+			</div><!--end of the four part-->
+
+
+
+		</div><!--end of row-->
+
+	</div><!--end of the serice pane area-->
+	
+
+
+<div id="featured" class="con">
+	<h2><span>Top Best Seller Property</span></h2>
+
+<div id="bestseller">
+	<div class="row" id="bestrow">
+		<div class="six">
+			<img src="IMAGES/thumb.png">
+		</div>
+		
+		<div class="six">
+			<h3>Name</h3>
+			
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			
+			<div id="bests_deails">
+			
+			<div class="rowbd">
+				<div class="three">
+					<p>nature</p>
+				</div>
+				
+				<div class="three">
+					<p>type</p>
+				</div>
+				
+				<div class="three">
+					<p>purpose</p>
+				</div>
+				
+				<div class="three">
+					<p>area</p>
+				</div>
+				
+			</div><!--end of row-->
+			
+			
+			<div class="rowbd">
+				<div class="three">
+					<p>beds</p>
+				</div>
+				
+				<div class="three">
+					<p>baths</p>
+				</div>
+				
+				<div class="six">
+					<p>price: 20,000,000 Kshs</p>
+				</div>
+				
+				
+			</div><!--end of row-->
+			<div id="clear"></div>
+			</div><!--end of best seler details-->
+		</div>
+	
+	</div>
+	
+
+	
+	
+	<div class="row">
+		<div class="six">
+			<img src="IMAGES/thumb.png">
+		</div>
+		
+		<div class="six">
+			<h3>The Title Is Here</h3>
+			
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+		</div>
+	
+	</div>
+	
+	
+	
+		<div class="row">
+		<div class="six">
+			<img src="IMAGES/thumb.png">
+		</div>
+		
+		<div class="six">
+			<h3>The Title Is Here</h3>
+			
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+			<p>I keep my description right here please. Please make a point of checking it if it mattersto you</p>
+		</div>
+	
+	</div>
+
+</div><!--end of best seller-->
+	
+		<div id="clear">
+		</div>
+</div><!--end of features-->
+
+
+	<div id="featured2" class="con">
+	
+		<h2><span>Featured Property In Nairobi and its Environs</span></h2>
+		<div id="featcon">
+		
+		<div class="four">	
+		<img src="IMAGES/thumb.png">
+			<div id='topinfo' class='row'>
+				<div class="row">
+					<div class="six">
+					sd
+					</div>
+					
+					<div class="three">
+					sdd
+					</div>
+					
+					<div class="three">
+					sddd
+					</div>
+				</div>
+				
+				<div class="row">
+				
+					<div class="three">
+					sd
+					</div>
+					
+					<div class="three">		
+					sdd					
+					</div>
+					
+					<div class="six">
+					ssd
+					</div>
+					
+				</div>	
+					
+			
+			</div><!--end of top inf-->
+			
+			
+			<div id="bottominfo">
+			
+				<div class="row">
+					<p>ddffgggg ggghh hhhhhh</p>
+				</div>
+				
+				<div class="row">	
+					<p>gjjgjgjgjg ggjgggngg ggn</p>
+				</div>
+			
+			</div><!--end of the bottom inf-->
+		
+		</div><!--end of four-->
+		
+		<div class="four">	
+		
+		</div>
+		
+		<div class="four">	
+		
+		</div>				
+		
+		<div class="four">	
+		
+		</div>
+		
+		<div class="four">	
+		
+		</div>
+		
+		<div class="four">	
+	
+		</div>		
+
+	</div>
+	
+	<div class="row">		
+	</div><!--end of row-->
+	</div><!--end of the featured area-->
+
+
+<div class="row">		
+</div><!--end of row-->
+
+	<div id="blogs" class="con">
+	<h2><span>News and Articles</span></h2>
+		<div class="row">
+			<div class="three">
+			<h3>Top 10 Best Selller Plots</h3>	
+			<p>This is one of the divisions that contain most of the information</p>
+				
+			</div>
+		
+		</div><!--end of row-->
+		
+	</div><!--end of blogs-->
+
+<div id="clear"></div>
+
+
+<div id="clear">
+		</div>
+
+<script>
+getfeaturedpr();
+getbestseller();
+</script>
+
+</div><!--end of content-->	
+		
+<?php
+include "templates/footer.php";	
+?>
+    
+    
+    
+        
+ 
+ 
+    
+
+    
